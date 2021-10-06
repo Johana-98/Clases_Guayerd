bbc573520bd27e930981dfed681ab22a57602933
Johana Quiroga <90170635+Johana-98@users.noreply.github.com>
2021-10-06 20:19:29 -0300

    Intro a JavaScript

---
+++ Clase 5/app.js
@@ -0,0 +1,15 @@
+// Bienvenida
+alert("Bievenide!! =)");
+
+// Vamos a llamar a este alert
+function primerBoton(){
+    alert("Hiciste click en el primer boton");
+}
+
+function segundoBoton(){
+    window.confirm("¿Te gusta JS?");
+}
+
+function tercerBoton(){
+    window.prompt("¿Como es tu nombre?");
+}
